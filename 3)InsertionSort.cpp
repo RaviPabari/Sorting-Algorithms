b0cce91c70c34caedcfb36615743ef1cc51cde99
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2019-09-11 01:05:17 +0530

    Insertion Sort

---
+++ 3)InsertionSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int A[],int n){
+	int x,i,j;							//Time Complexity = O(n^2)
+	for(i=1;i<n;i++){
+		j=i-1;
+		x = A[i];
+		while(j>-1 && A[j]>x){
+			A[j+1] = A[j];
+			j=j-1;
+		}
+		A[j+1] = x;
+	}
+}
+
+int main(){
+	int n=5;
+	int A[5]={43,23,125,87,32};
+	InsertionSort(A,n);
+	for(int i=0;i<n;i++){
+		cout << A[i] << endl;
+	}
+	return 0;
+}
