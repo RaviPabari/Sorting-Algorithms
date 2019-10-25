71e213f43d7f6b5d3b2f37eea62d037ce968639f
mayursinh059 <56933938+mayursinh059@users.noreply.github.com>
2019-10-25 14:50:42 +0530

    1) BubbbleSort.cpp

--- 1) Bubble.cpp
+++ 1) Bubble.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
+
 void swap(int *x, int *y){
 	int temp=*x;
 	*x=*y;
@@ -8,8 +9,7 @@ void swap(int *x, int *y){
 }
 
 void BubbleSort(int A[], int n){
-	int i,j,flag=0;
-	
+	int i,j,flag=0;	
 	for(i=0;i<n;i++){
 		flag=0;
 		for(j=0;j<n-i-1;j++){
@@ -26,8 +26,7 @@ void BubbleSort(int A[], int n){
 
 int main(){
 	int A[] = {11,13,6,23,443,44,54,656,65,98}, n=10,i;
-	BubbleSort(A,n);
-	
+	BubbleSort(A,n);	
 	for(i=0;i<n;i++){
 		cout << A[i] << " " ;
 	}
